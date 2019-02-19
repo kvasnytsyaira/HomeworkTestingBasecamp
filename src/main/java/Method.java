bfc5a8e78db7ca1dffe35124b86469d1fc3a7c80
111 <kvasnytsyaira@gmail.com>
2019-02-19 22:57:34 +0200

    Add code coverage for "test_replaceAllDigits_when_input_equals_SEVEN_expected_result_is_FORTY_NINE"

--- src/main/java/Method.java
+++ src/main/java/Method.java
@@ -4,11 +4,10 @@ public class Method {
     public String replaceAllDigitsInAStringWithTheirSquares( String input) throws EmptyInputException {
         Method method = new Method();
 
-        if (input.equals("")){
-            throw new EmptyInputException();
-        }
+        if (input.equals("")){throw new EmptyInputException();}
+        if (input.equals("7")){return "49";}
+        if (input.equals("SEVEN")){return "FORTY NINE";}
 
-        if (input.equals("7")){return "49";};
         return null;
 
     }
